456c95c8b23d9c21c7364c95366b3cb6576ee8e5
bd <51322242+bdwebnet@users.noreply.github.com>
2022-10-26 10:12:08 +0200

    Besprechung

--- src/net/bdweb/gds2/pr1_70_80_90_schleifen/ab_20_wiederholung/a01.java
+++ src/net/bdweb/gds2/pr1_70_80_90_schleifen/ab_20_wiederholung/a01.java
@@ -1,22 +1,24 @@
 package net.bdweb.gds2.pr1_70_80_90_schleifen.ab_20_wiederholung;
 
-public class a01 {
+import java.text.DecimalFormat;
 
-    public static final int wirkstoffgehalt_anfang = 100;
-    public static final int abnahme_pro_monat = 4;
-    public static final int grenzwert = 50;
+public class a01 {
+    public static final double dVerlust = 4;
+    public static final double dGrenzwert = 50;
 
     public static void main(String[] args) {
 
-        int wirkstoffgehalt = wirkstoffgehalt_anfang;
-        int monate = 0;
+        double dWirkstoffgehalt = 100;
+        int iMonate = 0;
 
-        while (wirkstoffgehalt >= grenzwert) {
-            wirkstoffgehalt -= abnahme_pro_monat;
-            monate += 1;
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        while (dWirkstoffgehalt >= dGrenzwert) {
+            dWirkstoffgehalt = dWirkstoffgehalt - dVerlust/100 * dWirkstoffgehalt;
+            iMonate += 1;
         }
 
-        System.out.println("Das Medikament hat nach " + monate + " Monaten " + wirkstoffgehalt + "% des Wirkstoffgehalts verloren.");
+        System.out.println("Das Medikament hat nach " + iMonate + " Monaten " + df.format(dWirkstoffgehalt) + " % des Wirkstoffgehalts verloren.");
 
     }
 
