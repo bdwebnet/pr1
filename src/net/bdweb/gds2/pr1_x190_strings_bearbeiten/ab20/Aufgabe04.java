4036097a0ecbc24a395649d2eab5dc9caba83090
bd <51322242+bdwebnet@users.noreply.github.com>
2022-12-08 10:42:33 +0100

    aufgaben

--- src/net/bdweb/gds2/pr1_x190_strings_bearbeiten/ab20/Aufgabe04.java
+++ src/net/bdweb/gds2/pr1_x190_strings_bearbeiten/ab20/Aufgabe04.java
@@ -1,5 +1,7 @@
 package net.bdweb.gds2.pr1_x190_strings_bearbeiten.ab20;
 
+import java.util.Random;
+
 public class Aufgabe04 {
 
     public static String ErzeugeZufaelligesPasswort(int length) {
@@ -12,9 +14,21 @@ public class Aufgabe04 {
 
         Random rand = new Random();
 
-        for (int i = 0; i < length; i++) {
+        if (length > 5) {
+            for (int i = 0; i < length; i++) {
+                char neuesZeichen = auswahl.toCharArray()[rand.nextInt(auswahl.length())];
 
+                passwort = passwort.concat(String.valueOf(neuesZeichen));
+            }
+        } else {
+            return "Bitte gebe eine Länge größer als 5 an.";
         }
+
+        return passwort;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ErzeugeZufaelligesPasswort(10));
     }
 
 }
