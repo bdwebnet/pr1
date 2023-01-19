38d64770ec8ea887390bcc9a358a443ba61ddacc
bd <51322242+bdwebnet@users.noreply.github.com>
2023-01-19 10:05:14 +0100

    Objekte in Objekte

---
+++ src/net/bdweb/gds2/pr1_x210_Ueberladen_von_Methoden/ab10_OinO/Autovermietung.java
@@ -0,0 +1,12 @@
+package net.bdweb.gds2.pr1_x210_Ueberladen_von_Methoden.ab10_OinO;
+
+public class Autovermietung {
+
+    public static void main ( String[] args ) {
+        Fahrzeug mercedes = new Fahrzeug();
+        Fahrzeug ferrari = new Fahrzeug();
+
+        Fuhrpark Car2T = new Fuhrpark(mercedes, ferrari);
+    }
+
+}
--- src/net/bdweb/gds2/pr1_x210_Ueberladen_von_Methoden/ab10_OinO/Fuhrpark.java
+++ src/net/bdweb/gds2/pr1_x210_Ueberladen_von_Methoden/ab10_OinO/Fuhrpark.java
@@ -3,7 +3,12 @@ package net.bdweb.gds2.pr1_x210_Ueberladen_von_Methoden.ab10_OinO;
 public class Fuhrpark {
 
     private int anzahl_stellplaetze;
-    private Fahrzeug sportwagen;
-    private Fahrzeug stadtwagen;
+    private final Fahrzeug sportwagen;
+    private final Fahrzeug stadtwagen;
+
+    public Fuhrpark ( Fahrzeug sportwagen, Fahrzeug stadtwagen ) {
+        this.sportwagen = sportwagen;
+        this.stadtwagen = stadtwagen;
+    }
 
 }
