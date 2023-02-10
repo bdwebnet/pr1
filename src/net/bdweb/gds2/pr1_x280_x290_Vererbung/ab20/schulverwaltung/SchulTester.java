aaf4e9d2496a50aab775dc31c22fe9723af88b3b
bd <51322242+bdwebnet@users.noreply.github.com>
2023-02-10 08:34:23 +0100

    bugfix

--- src/net/bdweb/gds2/pr1_x280_x290_Vererbung/ab20/schulverwaltung/SchulTester.java
+++ src/net/bdweb/gds2/pr1_x280_x290_Vererbung/ab20/schulverwaltung/SchulTester.java
@@ -6,40 +6,40 @@ public class SchulTester {
 
     public static void main ( String[] args ) {
 
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Du baust eine Schule!");
 
         System.out.println("Erstelle einen Menschen:");
-        String name = getScannedString("Bitte gebe einen Namen ein: ");
-        int alter = getScannedInt("Bitte gebe ein Alter ein: ");
+        String name = getScannedString(sc, "Bitte gebe einen Namen ein: ");
+        int alter = getScannedInt(sc, "Bitte gebe ein Alter ein: ");
         Mensch deinMensch = new Mensch(name, alter);
 
         System.out.println("Erstelle einen Lehrer:");
-        name = getScannedString("Bitte gebe einen Namen ein: ");
-        alter = getScannedInt("Bitte gebe ein Alter ein: ");
-        String fach = getScannedString("Bitte gebe ein Unterrichtsfach ein: ");
+        name = getScannedString(sc, "Bitte gebe einen Namen ein: ");
+        alter = getScannedInt(sc, "Bitte gebe ein Alter ein: ");
+        String fach = getScannedString(sc, "Bitte gebe ein Unterrichtsfach ein: ");
         Lehrer deinLehrer = new Lehrer(name, alter, fach);
 
         System.out.println("Erstelle einen Schüler:");
-        name = getScannedString("Bitte gebe einen Namen ein: ");
-        alter = getScannedInt("Bitte gebe ein Alter ein: ");
-        int stufe = getScannedInt("Bitte gebe eine Klassenstufe ein: ");
+        name = getScannedString(sc, "Bitte gebe einen Namen ein: ");
+        alter = getScannedInt(sc, "Bitte gebe ein Alter ein: ");
+        int stufe = getScannedInt(sc, "Bitte gebe eine Klassenstufe ein: ");
         Schueler deinSchueler = new Schueler(name, alter, stufe);
+
+        sc.close();
 
     }
 
-    public static int getScannedInt ( String question ) {
-        Scanner sc = new Scanner(System.in);
+    public static int getScannedInt ( Scanner sc, String question ) {
         System.out.print(question);
         int returnInt = sc.nextInt();
-        sc.close();
         return returnInt;
     }
 
-    public static String getScannedString ( String question ) {
-        Scanner sc = new Scanner(System.in);
+    public static String getScannedString ( Scanner sc, String question ) {
         System.out.print(question);
         String returnString = sc.next();
-        sc.close();
         return returnString;
     }
 
