e59a18a9e06e9301ffa58e6df7a90c4cd21121f0
bd <51322242+bdwebnet@users.noreply.github.com>
2023-03-10 08:06:10 +0100

    interfaces

--- src/net/bdweb/gds2/pr1_x280_x290_Vererbung/interfaces/a01/DasFuenfte.java
+++ src/net/bdweb/gds2/pr1_x280_x290_Vererbung/interfaces/a01/DasFuenfte.java
@@ -1,31 +1,32 @@
 package net.bdweb.gds2.pr1_x280_x290_Vererbung.interfaces.a01;
 
+import java.util.Vector;
+
 public class DasFuenfte implements NachrichtenQuelle {
 
-    private String nachricht;
-    private NachrichtenEmpfaenger empfaenger;
+    private String typ;
+    private Vector vec = new Vector<Person>();
+
+    public DasFuenfte ( String typ ) {
+        this.typ = typ;
+    }
 
     @Override
     public void anmelden ( NachrichtenEmpfaenger empf ) {
-        empfaenger = empf;
+        vec.add(empf);
     }
 
     @Override
     public void abmelden ( NachrichtenEmpfaenger empf ) {
-        empfaenger = null;
+        vec.remove(empf);
     }
 
     @Override
     public void sendeNachricht ( String nachricht ) {
-        empfaenger.empfangeNachricht(nachricht);
-    }
-
-    public String getNachricht () {
-        return nachricht;
-    }
-
-    public void setNachricht ( String nachricht ) {
-        this.nachricht = nachricht;
+        for ( int i = 0; i < vec.size(); i++ ) {
+            NachrichtenEmpfaenger ref = ( NachrichtenEmpfaenger ) vec.elementAt(i);
+            ref.empfangeNachricht(nachricht);
+        }
     }
 
 }
---
+++ src/net/bdweb/gds2/pr1_x280_x290_Vererbung/interfaces/a01/Starter.java
@@ -0,0 +1,18 @@
+package net.bdweb.gds2.pr1_x280_x290_Vererbung.interfaces.a01;
+
+public class Starter {
+
+    public static void main ( String[] args ) {
+
+        Person p1 = new Person("Muster", "Max");
+
+        DasFuenfte tv1 = new DasFuenfte("Das 5. TV");
+
+        tv1.anmelden(p1);
+        tv1.sendeNachricht("Das Fünfte ist kaputt.");
+
+        System.out.println("Test");
+
+    }
+
+}
