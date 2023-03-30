5f152397d1326014bfaa9769eb30e950f64f8833
bd <51322242+bdwebnet@users.noreply.github.com>
2023-03-30 12:11:12 +0200

    bugfix

--- src/net/bdweb/gds2/pr1_x425_Aggregation_Komposition/ab30/a01/Werkzeug.java
+++ src/net/bdweb/gds2/pr1_x425_Aggregation_Komposition/ab30/a01/Werkzeug.java
@@ -1,17 +1,21 @@
 package net.bdweb.gds2.pr1_x425_Aggregation_Komposition.ab30.a01;
 
-public class Werkzeug {
+public abstract class Werkzeug {
 
     protected int verschleiss = 0;
     private String art;
 
     public Werkzeug ( String art, int verschleiss ) {
         this.art = art;
+
+        if ( verschleiss < 0 )
+            verschleiss = 0;
+        else if ( verschleiss > 100 )
+            verschleiss = 100;
+
         this.verschleiss = verschleiss;
     }
 
-    public void ausgeben () {
-        System.out.println("Werkzeug: " + this.art + " mit Verschleiss " + this.verschleiss);
-    }
+    public abstract void ausgeben ();
 
 }
