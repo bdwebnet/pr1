0913c52572187a115b197a060b9cd8941345afcb
bd <51322242+bdwebnet@users.noreply.github.com>
2023-04-28 08:02:19 +0200

    JButton mit Icon

--- src/net/bdweb/gds2/pr1_x440_x350_GUI_Start/ab20_GUI_Java_JButton/ButtonBeispiel.java
+++ src/net/bdweb/gds2/pr1_x440_x350_GUI_Start/ab20_GUI_Java_JButton/ButtonBeispiel.java
@@ -8,13 +8,16 @@ public class ButtonBeispiel extends JFrame {
     JButton bSchliessen;
 
     public ButtonBeispiel ( String text ) throws HeadlessException {
-        
+
         super(text);
 
         this.setLayout(new FlowLayout());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        bSchliessen = new JButton("Schliessen");
+        ImageIcon image = new ImageIcon("src/net/bdweb/gds2/pr1_x440_x350_GUI_Start/ab20_GUI_Java_JButton/exit.png", "Schliessen");
+
+        bSchliessen = new JButton("Schliessen", new ImageIcon(image.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT)));
+        bSchliessen.addActionListener(e -> System.exit(0));
         this.add(bSchliessen);
 
     }
