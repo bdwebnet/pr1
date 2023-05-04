d7513f986a4632c117e12a366293da8504ab6a4a
bd <51322242+bdwebnet@users.noreply.github.com>
2023-05-04 11:10:42 +0200

    zahlenschloss u.a.

--- src/net/bdweb/gds2/pr1_x440_x350_GUI_Start/ab20_GUI_Java_JButton/ButtonBeispiel.java
+++ src/net/bdweb/gds2/pr1_x440_x350_GUI_Start/ab20_GUI_Java_JButton/ButtonBeispiel.java
@@ -17,7 +17,6 @@ public class ButtonBeispiel extends JFrame {
         ImageIcon image = new ImageIcon("src/net/bdweb/gds2/pr1_x440_x350_GUI_Start/ab20_GUI_Java_JButton/exit.png", "Schliessen");
 
         bSchliessen = new JButton("Schliessen", new ImageIcon(image.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT)));
-        bSchliessen.addActionListener(e -> System.exit(0));
         this.add(bSchliessen);
 
     }
---
+++ src/net/bdweb/gds2/pr1_x440_x350_GUI_Start/ab25_Tuerschloss/Tester.java
@@ -0,0 +1,11 @@
+package net.bdweb.gds2.pr1_x440_x350_GUI_Start.ab25_Tuerschloss;
+
+public class Tester {
+
+    public static void main ( String[] args ) {
+
+        new Zahlenschloss("Zahlenschloss");
+
+    }
+
+}
